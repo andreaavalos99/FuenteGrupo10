748c2c5e351f1b78827c0af79475ef37df751457
igalPR <ipistermanrozenek@frba.utn.edu.ar>
2025-09-07 19:19:24 -0300

    cambio en config

--- src/main/java/ar/edu/utn/dds/k3003/config/JacksonConfig.java
+++ src/main/java/ar/edu/utn/dds/k3003/config/JacksonConfig.java
@@ -2,6 +2,9 @@ package ar.edu.utn.dds.k3003.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +14,9 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         return mapper;
     }
-} 
+}
