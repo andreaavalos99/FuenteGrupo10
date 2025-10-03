4457d659558c07e9dd7c0b1c6bb6572e4901b18c
andreaavalos99 <andreaavalos99@gmal.com>
2025-10-03 16:24:07 -0300

    correccion del pdi

--- src/main/java/ar/edu/utn/dds/k3003/dto/PdiProcesadorDTO.java
+++ src/main/java/ar/edu/utn/dds/k3003/dto/PdiProcesadorDTO.java
@@ -5,17 +5,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class PdiProcesadorDTO {
-    private String id;
-    private String hechoId;
-    private String descripcion;
-    private String lugar;
-    private LocalDateTime momento;
-    private String contenido;
-    private List<String> etiquetas;
-    private String resultadoOcr;
-    private String urlImagen;
+public record PdiProcesadorDTO(
+        String id,
+        String hechoId,
+        String descripcion,
+        String lugar,
+        LocalDateTime momento,
+        String contenido,
+        List<String> etiquetas,
+        String resultadoOcr,
+        String urlImagen
+) {
+    public PdiProcesadorDTO(String id, String hechoId) {
+        this(id, hechoId, null, null, null, null, List.of(), null, null);
+    }
 }
