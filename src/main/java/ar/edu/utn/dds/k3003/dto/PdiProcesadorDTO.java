d8ce1906d04fb711dacd2b8d057fda564fdbdb25
andreaavalos99 <andreaavalos99@gmal.com>
2025-10-03 15:58:36 -0300

    correccion del pdi

--- src/main/java/ar/edu/utn/dds/k3003/dto/PdiProcesadorDTO.java
+++ src/main/java/ar/edu/utn/dds/k3003/dto/PdiProcesadorDTO.java
@@ -1,28 +1,21 @@
 package ar.edu.utn.dds.k3003.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Getter;
-import lombok.Setter;
-
+import lombok.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-@Setter
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record PdiProcesadorDTO(
-        String id,
-        String hechoId,
-        String descripcion,
-        String lugar,
-        LocalDateTime momento,
-        String contenido,
-        List<String> etiquetas,
-        String resultadoOcr,
-        String urlImagen
-) {
-    public PdiProcesadorDTO(String id, String hechoId) {
-        this(id, hechoId, null, null, null, null, List.of(), null, null);
-    }
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PdiProcesadorDTO {
+    private String id;
+    private String hechoId;
+    private String descripcion;
+    private String lugar;
+    private LocalDateTime momento;
+    private String contenido;
+    private List<String> etiquetas;
+    private String resultadoOcr;
+    private String urlImagen;
 }
